db5ad3662b5c0e63fcc0390e15f3be69e3d371d9
PyongyangRacer <murilomiglioli@yahoo.com.br>
2022-10-25 14:21:51 -0300

    Add project files.

---
+++ ChattingClient/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using ChattingInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChattingClient
+{
+
+    public partial class MainWindow : Window
+    {
+        public static IChattingService Server;
+        private static DuplexChannelFactory<IChattingService> _channelFactory;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _channelFactory = new DuplexChannelFactory<IChattingService>(new ClientCallback(), "ChattingServiceEndPoint");
+            Server = _channelFactory.CreateChannel();
+
+
+        }
+
+        public void TakeMessage(string message, string userName)
+        {
+            TextDisplayTextBox.Text += "[" + DateTime.Now.ToString("HH:mm:ss tt") + "]" + userName + ": " + message + "\n";
+            TextDisplayTextBox.ScrollToEnd();
+        }
+
+        private void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+            Server.SendMessageToALL(MessageTextBox.Text, UserNameTextBox.Text);
+            TakeMessage(MessageTextBox.Text, "Você");
+            MessageTextBox.Text = "";
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            int returnValue = Server.Login(UserNameTextBox.Text);
+            if (returnValue == 1)
+            {
+                MessageBox.Show("Você já está logado");
+            }
+            else if (returnValue == 0)
+            {
+                MessageBox.Show("Logado com sucesso!");
+                UserNameTextBox.IsEnabled = false;
+                LoginButton.IsEnabled = false;
+            }
+        }
+    }
+}
---
+++ ChattingServer/ChattingService.cs
@@ -0,0 +1,50 @@
+﻿using ChattingInterfaces;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ChattingServer
+{
+    [ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Multiple, InstanceContextMode = InstanceContextMode.Single)]
+    public class ChattingService : IChattingService
+    {
+        public ConcurrentDictionary<string, ConnectedClient> _connectedClients = new ConcurrentDictionary<string, ConnectedClient>();
+
+        public int Login(string userName)
+        {
+            foreach(var client in _connectedClients)
+            {
+                if(client.Key.ToLower() == userName.ToLower())
+                {
+                    return 1;
+                }
+            }
+
+            var establishedUserConnection = OperationContext.Current.GetCallbackChannel<IClient>();
+
+            ConnectedClient newClient = new ConnectedClient();
+            newClient.connection = establishedUserConnection;
+            newClient.UserName = userName;
+
+            _connectedClients.TryAdd(userName, newClient);
+
+            return 0;
+        }
+
+        public void SendMessageToALL(string message, string userName)
+        {
+            foreach (var client in _connectedClients)
+            {
+                if (client.Key.ToLower() != userName.ToLower())
+                {
+                    client.Value.connection.GetMessage(message, userName);
+                }
+            }
+        }
+
+    }
+}
